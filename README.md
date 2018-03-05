617a99625b02003e50b06c9fd3e0826e67a7e780
wenhanglei <3188744608@qq.com>
2018-03-05 15:11:33 +0800

    Reverse Integer

--- README.md
+++ README.md
@@ -4,6 +4,7 @@ My LeetCode Solution
 
 | # | Title | Solution | Difficulty |
 |---| ----- | -------- | ---------- |
+|4|[Reverse Integer](src/easy/math/ReverseInteger.java)|java|easy|
 |3|[Longest Substring Without Repeating Characters](src/medium/string/LongestSubstringWithoutRep.java)|java|Medium|
 |2|[Add Two Numbers](src/medium/linkedlist/AddTwoNumbers.java)|java|Medium|
 |1|[Two Sum](src/easy/array/TwoSum.java)|java|Easy|
---
+++ src/easy/math/ReverseInteger.java
@@ -0,0 +1,32 @@
+package easy.math;
+/**
+ * Given a 32-bit signed integer, reverse digits of an integer.
+ * @author wenhanglei
+ */
+public class ReverseInteger {
+	public int reverse(int x) {
+		//结果变量
+		int res = 0;
+		while(x != 0){
+			int a = x%10;
+			int b = res;
+			res = res * 10 + a;
+			if((res-a)/10 != b)
+				return 0;
+			x /= 10;
+		}
+        return res;
+    }
+	
+	
+	
+	/**
+	 * 测试函数
+	 */
+	public static void main(String[] args) {
+		int a = 1200;
+		ReverseInteger sol = new ReverseInteger();
+		int reverse = sol.reverse(a);
+		System.out.println(reverse);
+	}
+}
