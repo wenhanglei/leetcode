57ec4c47e740b26a6862dea6f939e94c6b5728b2
wenhanglei <3188744608@qq.com>
2018-03-10 14:15:08 +0800

    Longest Commmon Prefix

--- README.md
+++ README.md
@@ -4,6 +4,7 @@ My LeetCode Solution
 
 | # | Title | Solution | Difficulty |
 |---| ----- | -------- | ---------- |
+|7|[Longest Common Prefix](src/easy/string/LongestCommonPrefix.java)|java|Easy|
 |6|[Roman to Integer](src/medium/string/RomanToInteger.java)|java|Easy|
 |5|[Palindrome Number](src/easy/math/PalindromeNumber.java)|java|Easy|
 |4|[Reverse Integer](src/easy/math/ReverseInteger.java)|java|Easy|
---
+++ src/easy/string/LongestCommonPrefix.java
@@ -0,0 +1,45 @@
+package easy.string;
+/**
+ * Write a function to find the longest 
+ * common prefix string amongst an array of strings.
+ * @author wenhanglei
+ */
+public class LongestCommonPrefix {
+	/*
+	 * 思路：
+	 * 字典树
+	 */
+	public String longestCommonPrefix(String[] strs) {
+		StringBuilder res = new StringBuilder();
+		if(strs == null || strs.length == 0) return res.toString();
+		int num = Integer.MAX_VALUE;
+        for(int i = 0; i < strs.length; i++){
+        	num = Math.min(strs[i].length(), num);
+        }
+        for(int i = 0; i < num; i++){
+        	char ch = strs[0].charAt(i);
+        	for(int j = 1; j < strs.length; j++){
+        		if(ch != strs[j].charAt(i)){
+        			return res.toString();
+        		}
+        	}
+        	res.append(ch);
+        }
+        return res.toString();
+    }
+	
+	/**
+	 * 测试函数
+	 */
+	public static void main(String[] args) {
+		String[] strings = new String[4];
+		strings[0] = "I am a tiger.";
+		strings[1] = "I am your father.";
+		strings[2] = "I am your mother.";
+		strings[3] = "I am a rabbish.";
+		
+		LongestCommonPrefix sol = new LongestCommonPrefix();
+		String pre = sol.longestCommonPrefix(strings);
+		System.out.println(pre);
+	}
+}
