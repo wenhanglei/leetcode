a670171fe310c2cd8a7eb545a81b16f06c9b7434
wenhanglei <wenhanglei@gmail.com>
2018-03-19 10:40:11 +0800

    递归实现二分查找

--- src/easy/array/SearchInsertPosition.java
+++ src/easy/array/SearchInsertPosition.java
@@ -16,17 +16,33 @@ public class SearchInsertPosition {
         return binarySearch(nums, 0, nums.length-1, target);
     }
 	
+	/**
+	 * 二分查找的递归实现
+	 */
 	private int binarySearch(int[] nums, int lo, int hi, int target){
-		while(lo <= hi){
-			int mid = lo + (hi-lo)/2;
-			if(nums[mid] < target){
-				lo = mid+1;
-			}else if(nums[mid] > target){
-				hi = mid-1;
-			}else return mid;
-		}
-		return lo;
+		if(lo > hi) return lo;
+		int mid = lo + (hi-lo)/2;
+		if(nums[mid] < target){
+			return binarySearch(nums, mid+1, hi, target);
+		}else if(nums[mid] > target){
+			return binarySearch(nums, lo, mid-1, target);
+		}else return mid;
 	}
+	
+	/**
+	 * 二分查找的循环实现
+	 */
+//	private int binarySearch(int[] nums, int lo, int hi, int target){
+//		while(lo <= hi){
+//			int mid = lo + (hi-lo)/2;
+//			if(nums[mid] < target){
+//				lo = mid+1;
+//			}else if(nums[mid] > target){
+//				hi = mid-1;
+//			}else return mid;
+//		}
+//		return lo;
+//	}
 	
 	/**
 	 * 测试函数
@@ -34,7 +50,7 @@ public class SearchInsertPosition {
 	public static void main(String[] args) {
 		int[] arr1 = {1, 3, 5, 6};
 		SearchInsertPosition sol = new SearchInsertPosition();
-		int searchInsert = sol.searchInsert(arr1, 0);
+		int searchInsert = sol.searchInsert(arr1, 5);
 		System.out.println(searchInsert);
 	}
 }
