e1c77b11c8a526ce34c7c7c74cca02e2c85c2722
wenhanglei <wenhanglei@gmail.com>
2018-04-14 22:27:25 +0800

    factorial trailing

---
+++ src/easy/math/FactorialTrailingZeroes.java
@@ -0,0 +1,21 @@
+package easy.math;
+/**
+ * Given an integer n, return the number of trailing zeroes in n!.
+ * @author wenhanglei
+ */
+public class FactorialTrailingZeroes {
+	/*
+	 * 思路：
+	 * 
+	 */
+	public int trailingZeroes(int n) {
+        return 0;
+    }
+	
+	/**
+	 * 测试函数
+	 */
+	public static void main(String[] args) {
+		
+	}
+}
