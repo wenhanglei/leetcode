75189b1a175151d547d28915db3c0332cb9f5a01
wenhanglei <wenhanglei@gmail.com>
2018-05-06 22:55:39 +0800

    优化后的解法

--- src/easy/sort/ValidAnagram.java
+++ src/easy/sort/ValidAnagram.java
@@ -9,7 +9,11 @@ public class ValidAnagram {
 	 * 思路：
 	 */
 	public boolean isAnagram(String s, String t) {
-        return false;
+		int[] alphabet = new int[26];
+        for (int i = 0; i < s.length(); i++) alphabet[s.charAt(i) - 'a']++;
+        for (int i = 0; i < t.length(); i++) alphabet[t.charAt(i) - 'a']--;
+        for (int i : alphabet) if (i != 0) return false;
+        return true;
     }
 	
 	/**
