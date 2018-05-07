359bd9ee6a6e6d16be272ae7f583ff355eff5100
wenhanglei <wenhanglei@gmail.com>
2018-05-07 11:39:39 +0800

    判断一个字符串是否是另一个字符串颠倒次序后的结果

--- src/easy/sort/ValidAnagram.java
+++ src/easy/sort/ValidAnagram.java
@@ -7,19 +7,35 @@ package easy.sort;
 public class ValidAnagram {
 	/*
 	 * 思路：
+	 * 使用数组保存对应字符的出现次数
+	 * 先遍历原字符串统计所有字符的出现次数
+	 * 检查生成的字符串是否符合
 	 */
 	public boolean isAnagram(String s, String t) {
-		int[] alphabet = new int[26];
-        for (int i = 0; i < s.length(); i++) alphabet[s.charAt(i) - 'a']++;
-        for (int i = 0; i < t.length(); i++) alphabet[t.charAt(i) - 'a']--;
-        for (int i : alphabet) if (i != 0) return false;
-        return true;
+		//边界检查
+		if(s.length() != t.length()) return false;
+		int[] counts = new int[256];
+		int len = s.length();
+		for(int i = 0; i < len; i++){
+			char c = s.charAt(i);
+			counts[c] = counts[c] + 1;
+		}
+		for(int i = 0; i < len; i++){
+			char c = t.charAt(i);
+			if(counts[c] == 0) return false;
+			counts[c] = counts[c]-1;
+		}
+		return true;
     }
 	
 	/**
 	 * 测试函数
 	 */
 	public static void main(String[] args) {
-		
+		String s = "";
+		String t ="";
+		ValidAnagram sol = new ValidAnagram();
+		boolean res = sol.isAnagram(s, t);
+		System.out.println(res);
 	}
 }
