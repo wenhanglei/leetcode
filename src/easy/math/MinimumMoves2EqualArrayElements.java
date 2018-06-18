b98b35a823cdeca0b509420fe25684e6d06a1324
wenhanglei <wenhanglei@gmail.com>
2018-06-18 21:05:46 +0800

    使数组值相同最小移动距离

---
+++ src/easy/math/MinimumMoves2EqualArrayElements.java
@@ -0,0 +1,25 @@
+package easy.math;
+/**
+ * 
+ * @author wenhanglei
+ */
+public class MinimumMoves2EqualArrayElements {
+	/*
+	 * 思路：
+	 */
+	public int minMoves(int[] nums) {
+		int max = 0;                    //值最大的下标
+		
+        return 0;
+    }
+	
+	/**
+	 * 测试函数
+	 */
+	public static void main(String[] args) {
+		MinimumMoves2EqualArrayElements sol = new MinimumMoves2EqualArrayElements();
+		int[] nums = {1, 3, 3, 7};
+		int ret = sol.minMoves(nums);
+	}
+	
+}
