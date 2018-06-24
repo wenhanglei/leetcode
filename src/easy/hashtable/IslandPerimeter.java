e4561cf85c0ac5be0bf18554cccab14f8b761f9b
wenhanglei <wenhanglei@gmail.com>
2018-06-24 14:33:02 +0800

    计算小岛周长

--- src/easy/hashtable/IslandPerimeter.java
+++ src/easy/hashtable/IslandPerimeter.java
@@ -15,15 +15,36 @@ package easy.hashtable;
 public class IslandPerimeter {
 	/*
 	 * 思路：
+	 * 从左上角开始遍历
 	 */
 	public int islandPerimeter(int[][] grid) {
-        return 0;
+		int count = 0, overlaps = 0;                  //为1的个数和重复的计数
+		for(int i = 0; i < grid.length; i++){         //第i行
+			for(int j = 0; j < grid[i].length; j++){  //第j列
+				if(grid[i][j]==1){                    //如果这个数是小岛
+					count++;
+					//向下统计重复计算的个数
+					if(i < grid.length-1 && grid[i+1][j]==1) overlaps++;
+					//向右统计重复计算的个数
+					if(j < grid[i].length-1 && grid[i][j+1]==1) overlaps++;
+				}
+			}
+		}
+        return count*4-overlaps*2;
     }
 	
 	/**
 	 * 测试函数
 	 */
 	public static void main(String[] args) {
-		
+		IslandPerimeter sol = new IslandPerimeter();
+		int[][] grid = {
+				{0, 1, 0, 0},
+				{1, 1, 1, 0},
+				{0, 1, 0, 0},
+				{1, 1, 0, 0}
+		};
+		int ret = sol.islandPerimeter(grid);
+		System.out.println(ret);
 	}
 }
