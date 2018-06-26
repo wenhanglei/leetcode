c93231b06f2e990534c33c2365bd66a4b10ad472
wenhanglei <wenhanglei@gmail.com>
2018-06-26 21:16:53 +0800

    二分查找获取加热器最小覆盖距离

--- src/easy/binary_search/Heaters.java
+++ src/easy/binary_search/Heaters.java
@@ -1,4 +1,7 @@
 package easy.binary_search;
+
+import java.util.Arrays;
+
 /**
  * you are given positions of houses and heaters on 
  * a horizontal line, find out minimum radius of heaters 
@@ -8,12 +11,27 @@ package easy.binary_search;
 public class Heaters {
 	/*
 	 * 思路：
+	 * 遍历所有的house查找在heaters中最近的一个heater
+	 * 从所有取得的heater中找到最小值即为结果
 	 */
 	public int findRadius(int[] houses, int[] heaters) {
+		int min = 0;                                     //离当前house最近的一个heater
+		int max = 0;                                     //所有min中的最大值
+		Arrays.sort(heaters);                            //对heaters进行排序
 		for(int i = 0; i < houses.length; i++){
-			
+			if(houses[i] <= heaters[0])
+				min = heaters[0] - houses[i];
+			else if(houses[i] >= heaters[heaters.length-1])
+				min = houses[i] - heaters[heaters.length-1];
+			else {
+				if(Arrays.binarySearch(heaters, houses[i]) < 0){
+					int ip = -1-Arrays.binarySearch(heaters, houses[i]);
+					min = Math.min(heaters[ip]-houses[i], houses[i]-heaters[ip-1]);
+				}else min = 0;
+			}
+			if(min > max) max = min;
 		}
-        return 0;
+        return max;
     }
 	
 	/**
@@ -21,8 +39,8 @@ public class Heaters {
 	 */
 	public static void main(String[] args) {
 		Heaters sol = new Heaters();
-		int[] houses = {1, 2, 3, 4};
-		int[] heaters = {1, 4};
+		int[] houses = {1, 2, 3};
+		int[] heaters = {1, 2, 3};
 		int ret = sol.findRadius(houses, heaters);
 		System.out.println(ret);
 	}
