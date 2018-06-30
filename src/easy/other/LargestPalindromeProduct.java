305b99b9170fc36a997ab1a78b8c4fd6a3fc46d0
wenhanglei <wenhanglei@gmail.com>
2018-06-30 22:22:52 +0800

    最大回文数字

--- src/easy/other/LargestPalindromeProduct.java
+++ src/easy/other/LargestPalindromeProduct.java
@@ -9,6 +9,14 @@ public class LargestPalindromeProduct {
 	 * 思路：
 	 */
 	public int largestPalindrome(int n) {
+		//边界判断
+		if(n == 1)
+			return 9;
+		 //确定上下界
+		int upBound = (int) (Math.pow(10, n)-1);
+		int subBound = (int) ((Math.pow(10, n)-1)/10);
+		//最大值
+		
         return 0;
     }
 	
@@ -16,6 +24,8 @@ public class LargestPalindromeProduct {
 	 * 测试函数
 	 */
 	public static void main(String[] args) {
-		
+		LargestPalindromeProduct sol = new LargestPalindromeProduct();
+		int ret = sol.largestPalindrome(3);
+		System.out.println(ret);
 	}
 }
