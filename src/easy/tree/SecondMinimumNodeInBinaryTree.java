4d325307657f229a464d160dbe493f280456b902
wenhanglei <wenhanglei@gmail.com>
2018-08-11 23:04:56 +0800

    查找特定二叉树的第二小的节点值

--- src/easy/tree/SecondMinimumNodeInBinaryTree.java
+++ src/easy/tree/SecondMinimumNodeInBinaryTree.java
@@ -20,10 +20,22 @@ public class SecondMinimumNodeInBinaryTree {
 	public int findSecondMinimumValue(TreeNode root) {
 		if(root == null) return -1;
 		if(root.left == null && root.right == null) return -1;
-	}
-	
-	private int find(TreeNode node){
-		
+		if(root.left.val == root.val && root.right.val == root.val){
+			int left = findSecondMinimumValue(root.left);
+			int right = findSecondMinimumValue(root.right);
+			if(left == -1 && right == -1) return -1;
+			else if(left == -1) return right;
+			else if(right == -1) return left;
+			else return Math.min(left, right);
+		}else if(root.left.val == root.val){
+			int val = findSecondMinimumValue(root.left);
+			if(val == -1) return root.right.val;
+			else return Math.min(root.right.val, val);
+		}else if(root.right.val == root.val){
+			int val = findSecondMinimumValue(root.right);
+			if(val == -1) return root.left.val;
+			else return Math.min(root.left.val, val);
+		}else return -1;
 	}
 	
 	/**
