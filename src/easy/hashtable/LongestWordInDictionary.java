120fd2520972b2bb54fc5c0f93e92db3abcb1df2
wenhanglei <wenhanglei@gmail.com>
2018-08-28 22:49:31 +0800

    使用字典树实现

--- src/easy/hashtable/LongestWordInDictionary.java
+++ src/easy/hashtable/LongestWordInDictionary.java
@@ -1,7 +1,5 @@
 package easy.hashtable;
 
-import java.util.Arrays;
-import java.util.HashSet;
 
 /**
  * Given a list of strings words representing an English Dictionary, find the
@@ -15,23 +13,69 @@ import java.util.HashSet;
  */
 public class LongestWordInDictionary {
 	
+	private String ret = "";
+	private StringBuilder sb;
+	
 	/*
 	 * 思路：
 	 * 方案一：字典树（有点小复杂，本题就不用了）
 	 * 方案二：排序，保存所有合法的字符串
 	 */
 	public String longestWord(String[] words) {
-		Arrays.sort(words);
-		HashSet<String> set = new HashSet<>();
-		String ret = "";
-		for (String str : words) {
-			if(str.length() == 1 || set.contains(str.substring(0, str.length()-1))){
-				ret = str.length() > ret.length() ? str: ret;
-				set.add(str);
+		//创建字典树的根节点
+		TrieNode root = new TrieNode(null);
+		TrieNode curr = root;
+		//初始化字典树
+		for(int i = 0; i < words.length; i++){
+			for(int j = 0; j < words[i].length(); j++){
+				char ch = words[i].charAt(j);
+				if(curr.nodes[ch-97] == null)
+					curr.nodes[ch-97] = new TrieNode(ch);
+				curr = curr.nodes[ch-97];
 			}
+			if(!curr.isWord) curr.isWord = true;
+			curr = root;
 		}
+		//检索符合条件的结果
+		sb = new StringBuilder();
+		traverse(root);
 		return ret;
 	}
+	private void traverse(TrieNode node){
+		for(TrieNode n : node.nodes){
+			if(n != null && n.isWord){
+				sb.append(n.val);
+				traverse(n);
+			}
+		}
+		if(sb.length() > ret.length()) ret = sb.toString();
+		if(sb.length() > 0)sb.deleteCharAt(sb.length()-1);
+	}
+
+	
+	private class TrieNode{
+		private TrieNode[] nodes = new TrieNode[26];
+		private Character val;
+		private boolean isWord;
+		public TrieNode(Character val) {
+			this.val = val;
+		}
+	}
+	/*
+	 * 方案二：对字符串数组进行排序，保存所有合法的字符串
+	 */
+//	public String longestWord(String[] words) {
+//		Arrays.sort(words);
+//		HashSet<String> set = new HashSet<>();
+//		String ret = "";
+//		for (String str : words) {
+//			if(str.length() == 1 || set.contains(str.substring(0, str.length()-1))){
+//				ret = str.length() > ret.length() ? str: ret;
+//				set.add(str);
+//			}
+//		}
+//		return ret;
+//	}
 	
 	/**
 	 * 测试函数
