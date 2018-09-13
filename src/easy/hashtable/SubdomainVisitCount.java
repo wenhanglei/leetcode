2f576b3dd6ac87f13f27320e02ee5f50e61a9b16
wenhanglei <wenhanglei@gmail.com>
2018-09-13 21:38:33 +0800

    子域名访问次数统计

---
+++ src/easy/hashtable/SubdomainVisitCount.java
@@ -0,0 +1,25 @@
+package easy.hashtable;
+
+/**
+ * A website domain like "discuss.leetcode.com" consists of various subdomains.
+ * At the top level, we have "com", at the next level, we have "leetcode.com",
+ * and at the lowest level, "discuss.leetcode.com". When we visit a domain like
+ * "discuss.leetcode.com", we will also visit the parent domains "leetcode.com"
+ * and "com" implicitly.
+ * 
+ * @author wenhanglei
+ */
+public class SubdomainVisitCount {
+	/*
+	 * 思路：
+	 */
+	public List<String> subdomainVisits(String[] cpdomains) {
+
+	}
+	/**
+	 * 测试函数
+	 */
+	public static void main(String[] args) {
+		
+	}
+}
