e6a84ed2c9adf46d85f32cbf6847dd7ad5123c89
wenhanglei <wenhanglei@gmail.com>
2018-10-03 16:14:39 +0800

    机器人的移动模拟优化

--- src/easy/other/WalkingRobotSimulation.java
+++ src/easy/other/WalkingRobotSimulation.java
@@ -1,5 +1,8 @@
 package easy.other;
 
+import java.util.HashSet;
+import java.util.Set;
+
 /**
  * A robot on an infinite grid starts at point (0, 0) and faces north. The robot
  * can receive one of three possible types of commands:
@@ -12,27 +15,26 @@ public class WalkingRobotSimulation {
 	 */
 	public int robotSim(int[] commands, int[][] obstacles) {
 		int toward = 0;              //机器人的移动方向，0向上，1向右，2向下，3向左
-		int[] pos = {0, 0};
+		Set<String> obst = new HashSet<>();
+		for(int[] obstacle : obstacles){
+			obst.add(obstacle[0] + " " + obstacle[1]);
+		}
+		int x = 0, y = 0, ret = 0;
+		int[][] dirs = {{0, 1}, {1, 0}, {0, -1}, {-1, 0}};
 		for(int i = 0; i < commands.length; i++){
 			if(commands[i] == -1) toward = (toward+1)%4;
 			else if(commands[i] == -2) toward = (toward+3)%4;
 			else{
-				int preX = pos[0], preY = pos[1];
-				if(toward == 0) pos[1] += commands[i];
-				else if(toward == 1) pos[0] += commands[i];
-				else if(toward == 2) pos[1] -= commands[i];
-				else if(toward == 3) pos[0] -= commands[i];
-				
-				for(int[] obstacle : obstacles){
-					
+				while(commands[i]-- > 0){
+					if(!obst.contains((x + dirs[toward][0]) + " " + (y + dirs[toward][1]))){
+						x += dirs[toward][0];
+						y += dirs[toward][1];
+					}
 				}
+				ret = Math.max(ret, x*x + y*y);
 			}
 		}
-		
-	}
-	
-	private void move(int[] position, command, ){
-		
+		return ret;
 	}
 	
 	/**
@@ -41,7 +43,7 @@ public class WalkingRobotSimulation {
 	public static void main(String[] args) {
 		WalkingRobotSimulation sol = new WalkingRobotSimulation();
 		int[] commands = {4,-1,4,-2,4};
-		int[][] obstacles = {{2,4}};
+		int[][] obstacles = {{2, 4}};
 		int ret = sol.robotSim(commands, obstacles);
 		System.out.println(ret);
 	}
