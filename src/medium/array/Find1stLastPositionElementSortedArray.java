25118949067951cf249b2657538ed8f7cb980a6a
wenhanglei <wenhanglei@gmail.com>
2018-11-03 22:17:39 +0800

    求排序数组中给定值的起始位置

--- src/medium/array/Find1stLastPositionElementSortedArray.java
+++ src/medium/array/Find1stLastPositionElementSortedArray.java
@@ -1,5 +1,7 @@
 package medium.array;
 
+import java.util.Arrays;
+
 /**
  * Given an array of integers nums sorted in ascending order, find the starting
  * and ending position of a given target value.
@@ -10,16 +12,40 @@ package medium.array;
  */
 public class Find1stLastPositionElementSortedArray {
 	/*
-	 * 思路： 
+	 * 思路： 二分查找的变体形式
+	 * 1. 求得排序数组中大于或等于目标值的第一个值的索引
 	 */
 	public int[] searchRange(int[] nums, int target) {
-
+		//边界检查
+		int start = firstGreaterOrEqual(nums, target);
+		if(start == nums.length || nums[start] != target) return new int[]{-1, -1};
+		return new int[]{start, firstGreaterOrEqual(nums, target+1)-1};
+	}
+	
+	/**
+	 * 查找排序数组中的大于等于给定值的第一个数的下标
+	 * @param nums 升序排序数组
+	 * @param target 给定值
+	 * @return
+	 */
+	private int firstGreaterOrEqual(int[] nums, int target){
+		//边界检查
+		if(nums == null || nums.length == 0) return nums.length;
+		int lo = 0, hi = nums.length;
+		while(lo < hi){
+			int mid = lo + ((hi-lo)>>1);
+			if(nums[mid] < target) lo = mid + 1;
+			else hi = mid;
+		}
+		return lo;
 	}
 	
 	/**
 	 * 测试函数
 	 */
 	public static void main(String[] args) {
-		
+		Find1stLastPositionElementSortedArray sol = new Find1stLastPositionElementSortedArray();
+		int[] ret = sol.searchRange(new int[]{1}, 1);
+		System.out.println(Arrays.toString(ret));
 	}
 }
