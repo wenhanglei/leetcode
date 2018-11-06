e123d195a08cca7540cd4cb5da2b1cace0a054a3
wenhanglei <wenhanglei@gmail.com>
2018-11-06 18:39:54 +0800

    查找数组中和为指定值的数的集合算法优化

--- src/medium/array/CombinationSum.java
+++ src/medium/array/CombinationSum.java
@@ -1,9 +1,11 @@
 package medium.array;
 
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.HashSet;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Stack;
 
 /**
  * Given a set of candidate numbers (candidates) (without duplicates) and a
@@ -18,15 +20,41 @@ import java.util.List;
 public class CombinationSum {
 	/*
 	 * 思路：回溯法求得所有可能的结果集合，最后去重即可。
+	 * update:前面一种方案时间复杂度太高，需要改进。可以先对数组进行排序然后不回头地回溯，可以直接避免重复结果集
 	 */
 	public List<List<Integer>> combinationSum(int[] candidates, int target) {
 		List<List<Integer>> ret = new LinkedList<>();
 		//边界检查
 		if(candidates == null || candidates.length == 0) return ret;
-		help(candidates, ret, 0, target, "");
-		removeDuplicate(ret);
+		Arrays.sort(candidates);
+		Stack<Integer> stack = new Stack<>();
+		for(int i = 0; i < candidates.length; i++)
+			traverse(candidates, ret, stack, i, target, 0);
 		return ret;
 	}
+	
+	private void traverse(int[] candidates, List<List<Integer>> list, Stack<Integer> stack, int lo, int target, int sum){
+		//如果当前路劲所有数字和等于目标数字，则将栈中的结果添加到结果集中
+		stack.push(candidates[lo]);
+		sum += candidates[lo];
+		if(sum == target) {
+			list.add(new LinkedList<>(stack));
+		}else if(sum < target){
+			for(;lo < candidates.length; lo++){
+				traverse(candidates, list, stack, lo, target, sum);
+			}
+		}
+		stack.pop();
+	}
+	
+//	public List<List<Integer>> combinationSum(int[] candidates, int target) {
+//		List<List<Integer>> ret = new LinkedList<>();
+//		//边界检查
+//		if(candidates == null || candidates.length == 0) return ret;
+//		help(candidates, ret, 0, target, "");
+//		removeDuplicate(ret);
+//		return ret;
+//	}
 	
 	private void help(int[] candidates, List<List<Integer>> list, int sum, int target, String nums){
 		if(sum == target){
