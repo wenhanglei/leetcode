8eb064093f48cdc2d117e5ab25056bcda1cbed9f
wenhanglei <wenhanglei@gmail.com>
2018-11-14 15:45:22 +0800

    使用递归实现求幂算法

--- src/medium/math/Powxn.java
+++ src/medium/math/Powxn.java
@@ -12,16 +12,11 @@ public class Powxn {
 	public double myPow(double x, int n) {
 		//边界检查
 		if(n == 0) return 1;
-		double ret = x;
-		boolean flag = true;
-		if(n < 0) {
-			flag = false;
+		if(n < 0){
+			x = 1/x;
 			n = -n;
 		}
-		for(int i = 1; i < n; i++){
-			ret *= x;
-		}
-		return flag?ret:1/ret;
+		return n%2==0?myPow(x*x, n>>>1):x*myPow(x*x, n>>>1);
 	}
 	
 	/**
@@ -29,7 +24,7 @@ public class Powxn {
 	 */
 	public static void main(String[] args) {
 		Powxn sol = new Powxn();
-		double ret = sol.myPow(2, -2);
+		double ret = sol.myPow(2, -2147483648);
 		System.out.println(ret);
 	}
 }
