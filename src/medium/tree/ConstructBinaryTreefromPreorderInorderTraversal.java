4932bbf1c76ecf156e49892c42c3b625d29154ab
wenhanglei <wenhanglei@gmail.com>
2018-12-17 22:01:55 +0800

    根据先序和中序重建二叉树

--- src/medium/tree/ConstructBinaryTreefromPreorderInorderTraversal.java
+++ src/medium/tree/ConstructBinaryTreefromPreorderInorderTraversal.java
@@ -12,13 +12,6 @@ public class ConstructBinaryTreefromPreorderInorderTraversal {
 	public TreeNode buildTree(int[] preorder, int[] inorder) {
 		TreeNode node = new TreeNode(preorder[0]);
 	}
-	public TreeNode helper(int[] preorder, int[] inorder, int lo, int hi){
-		TreeNode node = new TreeNode(preorder[lo]);
-		int i = lo+1;
-		for(; i <= hi; i++) if(inorder[i] == preorder[lo]) break;
-		node.left = helper(preorder, inorder, lo, i-1);
-		node.right = helper(preorder, inorder, i+1, hi);
-	}
 	
 	/**
 	 * 测试函数
