787e5b549a7c833903ddbcaa0b4e24ad250c4151
wenhanglei <wenhanglei@gmail.com>
2018-12-23 11:33:53 +0800

    使用栈扁平化二叉树

--- src/medium/tree/FlattenBinaryTree2LinkedList.java
+++ src/medium/tree/FlattenBinaryTree2LinkedList.java
@@ -1,7 +1,7 @@
 package medium.tree;
 
-import java.util.ArrayList;
 import java.util.List;
+import java.util.Stack;
 
 /**
  * Given a binary tree, flatten it to a linked list in-place.
@@ -11,17 +11,34 @@ import java.util.List;
 public class FlattenBinaryTree2LinkedList {
 	/*
 	 * 思路：使用链表保存中序遍历即可
+	 * 方案二：使用栈来搞一哈
 	 */
 	public void flatten(TreeNode root) {
 		if(root == null) return;
-		List<TreeNode> list = new ArrayList<>();
-		helper(list, root);
-		for(int i = 0; i < list.size()-1; i++){
-			list.get(i).left = null;
-			list.get(i).right = list.get(i+1);
+		Stack<TreeNode> stack = new Stack<>();
+		stack.push(root);
+		TreeNode pre = null;
+		while(!stack.isEmpty()){
+			TreeNode curr = stack.pop();
+			if(pre != null){
+				pre.left = null;
+				pre.right = curr;
+			}
+			if(curr.right != null) stack.push(curr.right);
+			if(curr.left != null) stack.push(curr.left);
+			pre = curr;
 		}
-		list.get(list.size()-1).left = null;
 	}
+//	public void flatten(TreeNode root) {
+//		if(root == null) return;
+//		List<TreeNode> list = new ArrayList<>();
+//		helper(list, root);
+//		for(int i = 0; i < list.size()-1; i++){
+//			list.get(i).left = null;
+//			list.get(i).right = list.get(i+1);
+//		}
+//		list.get(list.size()-1).left = null;
+//	}
 	
 	private void helper(List<TreeNode> list, TreeNode node){
 		if(node == null) return;
