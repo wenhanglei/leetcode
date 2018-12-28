2a6eaaf78b202af4be92da9289d4cb4647501090
wenhanglei <wenhanglei@gmail.com>
2018-12-28 18:29:34 +0800

    使用队列解决单词阶梯

--- src/medium/other/WordLadder.java
+++ src/medium/other/WordLadder.java
@@ -1,9 +1,10 @@
 package medium.other;
 
 import java.util.Arrays;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.Queue;
 
-import utils.ArrayUtil;
 
 /**
  * Given two words (beginWord and endWord), and a dictionary's word list, find
@@ -13,53 +14,57 @@ import utils.ArrayUtil;
  * @author wenhanglei
  */
 public class WordLadder {
-	int min = Integer.MAX_VALUE;
 	/*
 	 * 思路： 广度优先遍历
 	 */
 	public int ladderLength(String beginWord, String endWord,
 			List<String> wordList) {
-		int m = wordList.size();
-		for(int i = 0; i < m; i++){
-			boolean[] marked = new boolean[m];
-			if(beginWord.equals(wordList.get(i))) {
-				marked[i] = true;
-				continue;
-			}
-			if(isValid(wordList.get(i), beginWord)) helper(endWord, i, wordList, marked, 2);
+		Queue<String> queue = new LinkedList<>();
+		boolean[] marked = new boolean[wordList.size()];
+		for(int i = 0; i < wordList.size(); i++){
+			if(beginWord.equals(wordList.get(i))) marked[i] = true;
 		}
-		return min == Integer.MAX_VALUE?0:min;
+		int count = 1;
+		queue.offer(beginWord);
+		queue.offer(null);
+		while(!queue.isEmpty()){
+			String str = queue.poll();
+			if(str != null){
+				if(str.equals(endWord)) return count;
+				for(int i = 0; i < wordList.size(); i++){
+					if(!marked[i]){
+						String word = wordList.get(i);
+						if(isValid(str, word)) {
+							marked[i] = true;
+							queue.add(word);
+						}
+					}
+				}
+			}else {
+				count++;
+				if(queue.peek() != null) queue.add(null);
+			}
+			
+		}
+		return 0;
 	}
 	
-	private void helper(String endWord, int idx, List<String> wl, boolean[] marked, int cnt){
-		if(endWord.equals(wl.get(idx))) {
-			min = Math.min(min, cnt);
-			return;
-		}
-		marked[idx] = true;
-		for(int i = 0; i < marked.length; i++){
-			if(!marked[i]){
-				String str = wl.get(idx);
-				String word = wl.get(i);
-				if(isValid(str, word)) helper(endWord, i, wl, marked, cnt+1);
-			}
-		}
-	}
-	
-	private boolean isValid(String str, String word){
+	private boolean isValid(String str1, String str2){
 		int count = 0;
-		for(int i = 0; i < str.length(); i++){
-			if(str.charAt(i)-word.charAt(i) != 0) count++;
+		for(int i = 0; i < str1.length(); i++){
+			if(count > 1) return false;
+			if(str1.charAt(i)-str2.charAt(i) != 0) count++;
 		}
 		return count == 1?true:false;
 	}
+	
 	
 	/**
 	 * 测试函数
 	 */
 	public static void main(String[] args) {
 		String beginWord = "hit";
-		String endWord = "cog";
+		String endWord = "hot";
 		List<String> wordList = Arrays.asList(new String[]{"hot","dot","dog","lot","log","cog"});
 		WordLadder sol = new WordLadder();
 		int ret = sol.ladderLength(beginWord, endWord, wordList);
