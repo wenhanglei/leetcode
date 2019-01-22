afd88dda96c6a3de934afe39cc6289063c5bf970
wenhanglei <wenhanglei@gmail.com>
2019-01-22 21:12:43 +0800

    动态规划求最小积向量

--- src/medium/array/MaximumProductSubarray.java
+++ src/medium/array/MaximumProductSubarray.java
@@ -13,10 +13,19 @@ public class MaximumProductSubarray {
 	public int maxProduct(int[] nums) {
 		//边界检查
 		if(nums.length == 1) return nums[0];
-		int pre = nums[0], max = Integer.MIN_VALUE;
+		int[][] dp = new int[2][nums.length];
+		if(nums[0] > 0) dp[0][0] = nums[0];
+		else if(nums[0] < 0) dp[1][0] = nums[0];
+		int max = dp[0][0];
 		for(int i = 1; i < nums.length; i++){
-			pre = pre == 0? nums[i]: nums[i]*pre;
-			max = Math.max(pre, max);
+			if(nums[i] > 0){
+				dp[0][i] = dp[0][i-1] == 0? nums[i]: dp[0][i-1]*nums[i];
+				dp[1][i] = dp[1][i-1] == 0? 0: dp[1][i-1]*nums[i];
+			}else if(nums[i] < 0){
+				dp[0][i] = dp[1][i-1] == 0? 0: dp[1][i-1]*nums[i];
+				dp[1][i] = dp[0][i-1] == 0? nums[i]: dp[0][i-1]*nums[i];
+			}
+			max = Math.max(max, dp[0][i]);
 		}
 		return max;
 	}
@@ -25,7 +34,7 @@ public class MaximumProductSubarray {
 	 * 测试函数
 	 */
 	public static void main(String[] args) {
-		int[] nums = {3, -1, 4};
+		int[] nums = {-4, -3};
 		MaximumProductSubarray sol = new MaximumProductSubarray();
 		int ret = sol.maxProduct(nums);
 		System.out.println(ret);
