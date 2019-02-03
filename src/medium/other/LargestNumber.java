26abfd4d6f75206e2a37f83e36bba258c57f93ec
wenhanglei <wenhanglei@gmail.com>
2019-02-03 22:33:18 +0800

    使用一般排序构造最大数

--- src/medium/other/LargestNumber.java
+++ src/medium/other/LargestNumber.java
@@ -1,6 +1,7 @@
 package medium.other;
 
 import java.util.Arrays;
+import java.util.Comparator;
 import java.util.LinkedList;
 
 /**
@@ -11,13 +12,31 @@ import java.util.LinkedList;
  */
 public class LargestNumber {
 	/*
-	 * 思路：基数排序实现即可
+	 * 思路：一般的排序实现即可
 	 */
 	public String largestNumber(int[] nums) {
+		//边界检查
+		if(nums == null || nums.length == 0) return "";
+		
 		String[] strs = new String[nums.length];
 		for(int i = 0; i < nums.length; i++) strs[i] = Integer.toString(nums[i]);
 		
+		Comparator<String> cmp = new Comparator<String>() {
+			@Override
+			public int compare(String o1, String o2) {
+				if(o1.compareTo(o2) > 0) return -1;
+				else if(o1.compareTo(o2) < 0) return 1;
+				else return 0;
+			}
+		};
 		
+		Arrays.sort(strs, cmp);
+		
+		StringBuilder sb = new StringBuilder();
+		for(String str: strs){
+			sb.append(str);
+		}
+		return sb.toString();
 	}
 	
 	/**
