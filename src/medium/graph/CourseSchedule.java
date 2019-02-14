835fde6b6120fa2bc2c13aa7e02b19c3e73e3aff
wenhanglei <wenhanglei@gmail.com>
2019-02-14 14:25:37 +0800

    排课规划

--- src/medium/graph/CourseSchedule.java
+++ src/medium/graph/CourseSchedule.java
@@ -9,34 +9,23 @@ package medium.graph;
  * @author wenhanglei
  */
 public class CourseSchedule {
-	boolean hasCircle = false;
-	boolean[] onStack;          //当前路径已经遍历过的节点
 	/*
 	 * 思路：先决条件其实就是入度邻接表
 	 */
 	public boolean canFinish(int numCourses, int[][] prerequisites) {
 		//边界检查
-		if(numCourses == 1) return true;
-		if(prerequisites == null || prerequisites.length == 0) return false;
-		boolean[] marked = new boolean[numCourses];
-		onStack = new boolean[numCourses];
-		dfs(prerequisites, marked, 0);
-		if(hasCircle) return false;
-		for(boolean b : marked) if(!b) return false;
+		if(numCourses == 1 || prerequisites.length == 0) return true;
+		boolean flag = prerequisites[0][0]-prerequisites[0][1] > 0;
+		for(int[] edge : prerequisites){
+			if(flag){
+				if(edge[0] < edge[1]) return false;
+			}else{
+				if(edge[0] > edge[1]) return false;
+			}
+		}
 		return true;
 	}
 	
-	private void dfs(int[][] grid, boolean[] marked, int v){
-		if(hasCircle) return;            //如果已经有环了就不再遍历了
-		marked[v] = true;
-		onStack[v] = true;
-		for(int w : grid[v]){
-			if(!marked[v]) {
-				dfs(grid, marked, w);
-			}else if(onStack[w]) hasCircle = true;
-		}
-		onStack[v] = false;
-	}
 
 	/**
 	 * 测试函数
