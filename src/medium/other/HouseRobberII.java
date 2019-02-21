4c8d2d4b86deb2cda3432f446fcecd0808b7ca73
wenhanglei <wenhanglei@gmail.com>
2019-02-21 11:31:34 +0800

    求不相邻最大和

--- src/medium/other/HouseRobberII.java
+++ src/medium/other/HouseRobberII.java
@@ -14,16 +14,31 @@ package medium.other;
  */
 public class HouseRobberII {
 	/*
-	 * 思路：
+	 * 思路：使用动态规划解决
 	 */
 	public int rob(int[] nums) {
-		
+		//边界检查
+		if(nums.length == 1) return nums[0];
+		int sumToPre = 0;         //以当前数前一个数结尾的最大和
+		int sumToPrepre = 0;      //以当前数前两个数结尾的最大和
+		for(int i = 0; i < nums.length; i++){
+			sumToPrepre += nums[i];
+			int temp = sumToPre;
+			sumToPre = sumToPrepre;
+			sumToPrepre = temp;
+		}
+		if(nums.length%2 != 0) {
+			sumToPre -= Math.min(nums[0], nums[nums.length-1]);
+		}
+		return Math.max(sumToPre, sumToPrepre);
 	}
 	
 	/**
 	 * 测试函数
 	 */
 	public static void main(String[] args) {
-		
+		HouseRobberII sol = new HouseRobberII();
+		int ret = sol.rob(new int[]{2, 3, 2});
+		System.out.println(ret);
 	}
 }
