f2db161a4f0981a286ffbf1e7bc265f85321afd4
wenhanglei <wenhanglei@gmail.com>
2019-02-22 13:59:48 +0800

    小偷问题

--- src/medium/other/HouseRobberII.java
+++ src/medium/other/HouseRobberII.java
@@ -14,31 +14,42 @@ package medium.other;
  */
 public class HouseRobberII {
 	/*
-	 * 思路：使用动态规划解决
+	 * 思路：使用动态规划解决，个数为奇数和偶数分别处理
 	 */
 	public int rob(int[] nums) {
-		//边界检查
-		if(nums.length == 1) return nums[0];
-		int sumToPre = 0;         //以当前数前一个数结尾的最大和
-		int sumToPrepre = 0;      //以当前数前两个数结尾的最大和
-		for(int i = 0; i < nums.length; i++){
-			sumToPrepre += nums[i];
-			int temp = sumToPre;
-			sumToPre = sumToPrepre;
-			sumToPrepre = temp;
+		// 边界检查
+		if (nums.length == 1)
+			return nums[0];
+		int oddSum = 0;
+		int sinSum = 0;
+		if (nums.length % 2 == 0) {
+			for (int i = 0; i < nums.length; i++) {
+				if (i % 2 == 0)
+					oddSum += nums[i];
+				else
+					sinSum += nums[i];
+			}
+			return Math.max(oddSum, sinSum);
+		} else {
+			for (int i = 0; i < nums.length - 1; i++) {
+				if (i % 2 == 0)
+					oddSum += nums[i];
+				else
+					sinSum += nums[i];
+			}
+			sinSum = Math.max(
+					(sinSum - nums[nums.length - 2] + nums[nums.length - 1]),
+					sinSum);
+			return Math.max(sinSum, oddSum);
 		}
-		if(nums.length%2 != 0) {
-			sumToPre -= Math.min(nums[0], nums[nums.length-1]);
-		}
-		return Math.max(sumToPre, sumToPrepre);
 	}
-	
+
 	/**
 	 * 测试函数
 	 */
 	public static void main(String[] args) {
 		HouseRobberII sol = new HouseRobberII();
-		int ret = sol.rob(new int[]{2, 3, 2});
+		int ret = sol.rob(new int[] { 1, 3, 1, 3, 100 });
 		System.out.println(ret);
 	}
 }
