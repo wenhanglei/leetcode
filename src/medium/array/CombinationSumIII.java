b09ef558f2e64b22d4e37c963df13b5694e836c2
wenhanglei <wenhanglei@gmail.com>
2019-02-27 11:22:21 +0800

    回溯法求和值组合

--- src/medium/array/CombinationSumIII.java
+++ src/medium/array/CombinationSumIII.java
@@ -1,5 +1,6 @@
 package medium.array;
 
+import java.util.LinkedList;
 import java.util.List;
 
 /**
@@ -11,16 +12,35 @@ import java.util.List;
  */
 public class CombinationSumIII {
 	/*
-	 * 思路：
+	 * 思路：回溯法并且当前可取的数不能小于当前数
 	 */
 	public List<List<Integer>> combinationSum3(int k, int n) {
-
+		List<List<Integer>> ret = new LinkedList<>();
+		for(int i = 1; i < n && i < 10; i++){
+			List<Integer> list = new LinkedList<>();
+			helper(k-1, n-i, i, ret, list);
+		}
+		return ret;
+	}
+	
+	private void helper(int k, int n, int i, List<List<Integer>> ret, List<Integer> list){
+		list.add(i);
+		if(k == 0 && n == 0) {
+			ret.add(new LinkedList<>(list));
+		}else if(k != 0 && n != 0) {
+			for(int j = i+1; j < n+1 && j < 10; j++){
+				helper(k-1, n-j, j, ret, list);
+			}
+		}
+		list.remove(list.size()-1);
 	}
 	
 	/**
 	 * 测试函数
 	 */
 	public static void main(String[] args) {
-		
+		CombinationSumIII sol = new CombinationSumIII();
+		List<List<Integer>> ret = sol.combinationSum3(2, 6);
+		System.out.println(ret);
 	}
 }
