ab109ba216e358a912fba8c543d10e6d4ee83774
wenhanglei <wenhanglei@gmail.com>
2019-03-12 22:56:47 +0800

    使用中序遍历求二叉树中第k小的值

--- src/medium/tree/KthSmallestElementinBST.java
+++ src/medium/tree/KthSmallestElementinBST.java
@@ -7,17 +7,27 @@ package medium.tree;
  * @author wenhanglei
  */
 public class KthSmallestElementinBST {
+	Integer ret;
+	int count = 0;
 	/*
-	 * 思路：求第k小的值
+	 * 思路：方案一中序遍历即可
 	 */
 	public int kthSmallest(TreeNode root, int k) {
-
+		inorder(root, k);
+		return ret;
+	}
+	
+	private void inorder(TreeNode node, int k){
+		if(node ==null || ret == null) return;
+		inorder(node.left, k);
+		count++;
+		if(count == k) ret = node.val;
+		inorder(node.right, k);
 	}
 	
 	/**
 	 * 测试函数
 	 */
 	public static void main(String[] args) {
-		
 	}
 }
